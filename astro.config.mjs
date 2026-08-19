e803d42590bd16b11dc54437e0015836644bff17
vanssnii <vanssnii@gmail.com>
2026-08-19 09:29:17 -0400

    feat: Actualizaciones de version y menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 4321,
+  },
+  vite: {
+    server: {
+      headers: {
+        'Cache-Control': 'no-store',
+      },
+      hmr: {
+        host: '127.0.0.1',
+        protocol: 'ws',
+      },
+    },
+  },
+});
--- src/content/menu/map-disponible-true-nombre-flat-white-categoria-cafés-descripcion-café-cargado-con-un-toque-de-leche-precio-4500.md
+++ src/content/menu/map-disponible-true-nombre-flat-white-categoria-cafés-descripcion-café-cargado-con-un-toque-de-leche-precio-4500.md
@@ -3,5 +3,6 @@ nombre: Flat white
 categoria: Cafés
 descripcion: Café cargado con un toque de leche
 precio: "4500"
+imagen: /images/uploads/flat-white.jpg
 disponible: true
 ---
